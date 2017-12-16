e6b40a9791736f1492292a7c04ede675e9a0b6a2
ricvolpi <riccardo.volpi@aol.com>
2017-12-15 16:40:12 -0800

    Paper architecture

--- Model.py
+++ Model.py
@@ -18,20 +18,20 @@ class Model(object):
 	
 	
 	   
-    def encoder(self, images, reuse=False, return_fc4=False, is_training=False):
+    def encoder(self, images, reuse=False, return_fc4=False, is_training=False, from_features = False):
 	
 	with tf.variable_scope('encoder', reuse=reuse):
-	    with slim.arg_scope([slim.fully_connected], activation_fn=tf.nn.relu):
-		with slim.arg_scope([slim.conv2d], activation_fn=tf.nn.relu, padding='VALID'):
-		    net = slim.conv2d(images, 64, 5, scope='conv1')
-		    net = slim.max_pool2d(net, 2, stride=2, scope='pool1')
-		    net = slim.conv2d(net, 128, 5, scope='conv2')
-		    net = slim.max_pool2d(net, 2, stride=2, scope='pool2')
+	    with slim.arg_scope([slim.fully_connected], activation_fn=tf.nn.elu):
+		with slim.arg_scope([slim.conv2d], activation_fn=tf.nn.elu, padding='VALID'):
+		    
+		    net = slim.conv2d(images, 32, 8, scope='conv1')
+		    net = slim.conv2d(net, 64, 6, scope='conv2')
+		    net = slim.conv2d(net, 128, 5, scope='conv3')
 		    net = tf.contrib.layers.flatten(net)
-		    net = slim.fully_connected(net, 1024, scope='fc3')
-		    net = slim.fully_connected(net, self.embedding_size, activation_fn=tf.nn.relu, scope='fc4')
+		    net = slim.fully_connected(net, 1024, scope='fc1')
 		    net = slim.fully_connected(net, self.no_classes, activation_fn=None, scope='fc5')
 		    return net
+		
 		
 		
     def build_model(self):
